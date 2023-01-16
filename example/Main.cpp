1857cf1d334bbacb912b4a4c61bb6735914ffb5f
Malte Dömer <maltedoemer@gmail.com>
2023-01-16 17:10:34 +0100

    created test app for robot

--- example/Main.cpp
+++ example/Main.cpp
@@ -17,59 +17,70 @@ FILE* DebugFile;
 class App : public txt::application {
 
 public:
-    App() : button(txt, txt::I1), m1(txt, txt::M1), m2(txt, txt::M2)
+    App() :
+        button(txt, txt::I1),
+        ultrasonic(txt, txt::I6),
+        m1(txt, txt::M1),
+        m2(txt, txt::M2),
+        lamp1(txt, txt::O7),
+        lamp2(txt, txt::O8)
     {
-        // txt.configure_motor_output(txt::M1, true);
-        // txt.configure_motor_output(txt::M2, true);
     }
 
-    void setup() override
-    {
-        // txt.set_motor_distance(txt::M1, 250);
-        // txt.set_motor_distance(txt::M2, 250);
-        // txt.set_motor_master(txt::M1, 0);
-        // txt.set_motor_master(txt::M2, 1);
-
-        // txt.set_motor(txt::M1, 512, txt::direction::forward);
-        // txt.set_motor(txt::M2, 512, txt::direction::forward);
-
-        // txt.start_motor_command(txt::M1);
-        // txt.start_motor_command(txt::M2);
-
-        // printf("started command...\n");
-
-        // while (!txt.is_motor_command_finished(txt::M1) && !txt.is_motor_command_finished(txt::M2)) {
-        //     printf("not finished\n");
-        //     usleep(500000);
-        // }
-
-        // printf("finished\n");
-    }
+    void setup() override {}
 
     void update() override
     {
         usleep(100000);
 
         button.update();
+        ultrasonic.update();
+
+        if (ultrasonic.has_changed()) {
+            printf("d = %dcm\n", ultrasonic.distance());
+        }
 
         if (button.has_released()) {
-            m1.enhanced_mode(250, 0);
-            m2.enhanced_mode(250, 1);
-            m1.start(256, txt::direction::forward);
-            m2.start(256, txt::direction::forward);
-            printf("started command...\n");
-
-            while (!m1.has_stopped() && !m2.has_stopped()) {
-                printf("not finished\n");
-                usleep(500000);
-            }
+            toggle_lamps();
+            test_motors(500);
         }
     }
 
+    void toggle_lamps()
+    {
+        lamps_on = !lamps_on;
+
+        if (lamps_on) {
+            lamp1.on(512);
+            lamp2.on(512);
+        } else {
+            lamp1.off();
+            lamp2.off();
+        }
+    }
+
+    void test_motors(int distance)
+    {
+        m1.enhanced_mode(distance, 0);
+        m2.enhanced_mode(distance, 1);
+        m1.start(512, txt::direction::forward);
+        m2.start(512, txt::direction::forward);
+        printf("started command...\n");
+
+        while (!m1.has_stopped() && !m2.has_stopped()) {}
+
+        printf("finished\n");
+    }
+
 private:
-    txt::button button;
+    bool lamps_on = false;
+
     txt::motor m1;
     txt::motor m2;
+    txt::button button;
+    txt::pwm_output lamp1;
+    txt::pwm_output lamp2;
+    txt::ultrasonic ultrasonic;
 };
 
 int main()
